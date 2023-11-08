3e6390cb60bd549db9b4505affa104f9efc18f6b
Martin Berg Alstad <600878@stud.hvl.no>
2023-11-08 13:19:25 +0100

    Oppdatert pakker og testet nye GPT-4-turbo

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,4 @@
-type GPTModel = 'gpt-4' | 'gpt-3.5-turbo'
+type GPTModel = 'gpt-4' | 'gpt-3.5-turbo' | 'gpt-4-1106-preview'
 
 interface OpenAIRequest {
     system: string
@@ -6,4 +6,7 @@ interface OpenAIRequest {
     model?: GPTModel
 }
 
-interface OpenAIResponse {}
+interface OpenAIResponse {
+    role: 'system' | 'user' | 'assistant'
+    content: string
+}
